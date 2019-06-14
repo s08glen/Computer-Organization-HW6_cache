40e18a5baea2a5aee12b511d2a473c7288e0ccb7
s08glen <s08glen@gmail.comgit config --global user.email s08glen@gmail.comgit config --global user.email s08glen@gmail.com>
2019-06-14 20:23:59 +0800

    test

--- F74066242/src/cache.cpp
+++ F74066242/src/cache.cpp
@@ -1,4 +1,29 @@
 #include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<cstdio>
 using namespace std;
 
+void transfer(int* array,int){
 
+}
+
+int main(int argc, char **argv){
+	if(argc!=3){
+		cout << "Input error!\n";
+		return 0;
+	}
+	ifstream fin(argv[1]);
+	ofstream fout(argv[2]);
+	if (!fin){
+		cout << "Can not open file!\n";
+		return 0;
+	}
+	int csize,bsize,asso,replace;
+	fin>>csize;
+	fin>>bsize;
+	fic>>asso;
+	fin>>replace;
+	cout<<csize<<" "<<bsize<<" "<<asso<<" "<<replace<<endl;
+	return 0;
+}
