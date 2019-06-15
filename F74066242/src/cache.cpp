ee9f1d9b68fa28e3e7b94417e659d0bd99d86f12
s08glen <s08glen@gmail.comgit config --global user.email s08glen@gmail.comgit config --global user.email s08glen@gmail.com>
2019-06-15 22:54:36 +0800

    backup

--- F74066242/src/cache.cpp
+++ F74066242/src/cache.cpp
@@ -2,11 +2,54 @@
 #include<fstream>
 #include<cstdlib>
 #include<cstdio>
+#include<string>
+#include<cmath>
+#include<vector>
 using namespace std;
+struct caches{
+	bool valid;
+	int tag;
+	int block;
+};
 
-void transfer(int* array,int){
+vector<caches> cache;
 
+int bintodec(string sbin){
+	int iReturn = 0,count = 0;
+	for(int i=sbin.length()-1 ; i >= 0 ; i--){
+		if(sbin[i] == '1'){
+			iReturn += pow(2,count);
+		}
+		count++;
+	}
+	return iReturn;
 }
+
+string hextobin(string shex){
+	string sReturn = "";
+	for (int i=0 ; i<shex.length() ; i++){
+		switch (shex [i]){
+			case '0': sReturn.append ("0000"); break;
+			case '1': sReturn.append ("0001"); break;
+			case '2': sReturn.append ("0010"); break;
+			case '3': sReturn.append ("0011"); break;
+			case '4': sReturn.append ("0100"); break;
+			case '5': sReturn.append ("0101"); break;
+			case '6': sReturn.append ("0110"); break;
+			case '7': sReturn.append ("0111"); break;
+			case '8': sReturn.append ("1000"); break;
+			case '9': sReturn.append ("1001"); break;
+			case 'a': sReturn.append ("1010"); break;
+			case 'b': sReturn.append ("1011"); break;
+			case 'c': sReturn.append ("1100"); break;
+			case 'd': sReturn.append ("1101"); break;
+			case 'e': sReturn.append ("1110"); break;
+			case 'f': sReturn.append ("1111"); break;
+		}
+	}
+	return sReturn;
+}
+
 
 int main(int argc, char **argv){
 	if(argc!=3){
@@ -19,11 +62,32 @@ int main(int argc, char **argv){
 		cout << "Can not open file!\n";
 		return 0;
 	}
-	int csize,bsize,asso,replace;
+	int csize,bsize,asso,replace,temp,block,taglth,indexlth,tag,index;
+	int offset=4;//4 word = 16 bytes = 2^4  -> offset=4
+	string hexread,stag,sindex;
 	fin>>csize;
 	fin>>bsize;
-	fic>>asso;
+	fin>>asso;
 	fin>>replace;
-	cout<<csize<<" "<<bsize<<" "<<asso<<" "<<replace<<endl;
+	block = csize*1024/bsize;
+	struct cache ini;
+	ini.valid = 0;
+	ini.
+	for(int i=0;i<block;i++){
+		cache.push_back
+
+	}
+	indexlth = log(block)/log(2);
+	taglth = 32 - indexlth - offset;
+	//
+	while(fin>>hexread){
+		hexread = hexread.substr(2);	
+		hexread = hextobin(hexread);
+		stag = hexread.substr(0,taglth);
+		tag = bintodec(stag);
+		sindex = hexread.substr(taglth,indexlth);
+		index = bintodec(sindex);
+	}
+//	cout<<hex<<temp<<endl;
 	return 0;
 }
